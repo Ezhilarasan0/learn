84dd30b7d7ca54b98c7b8018c608e0494a3dba51
ezhilarasan0 <vipezhilarasan.com>
2023-12-17 13:35:05 +0530

    clock

--- Script.js
+++ Script.js
@@ -43,3 +43,96 @@
 
 // document.querySelector()
 // document.querySelectorAll()
+
+
+//HTMLCollection
+
+// let li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li.length);
+
+// let element = document.createElement("li");
+// element.innerHTML = "Javascript";
+
+// li[0].parentNode.appendChild(element);
+
+// let li = document.getElementsByTagName("li");
+// console.log(li);
+
+// let element = document.createElement("li");
+// element.innerHTML = "Javascript";
+
+// li[0].parentNode.appendChild(element);
+
+// console.log(element);
+// console.log(li);
+
+
+// const h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+
+// const parent_h1 = h1[0].parentNode;
+// parent_h1.style.background = "red";
+
+
+/*
+Style
+innerHTML
+innerText
+
+cloneNode()
+setInterval()
+*/
+
+
+// const h1 = document.querySelector("h1");
+// h1.style.color = "white";
+// h1.style.backgroundColor = "green";
+// h1.innerText = "Ezhilarasan Manjula"
+// h1.innerHTML = "<i>vip</i>Ezhilarasan Manjula";
+
+
+// const body = document.querySelector("body");
+// let cloneH1 = h1.cloneNode(true);
+// let cloneH2 = h1.cloneNode(false);
+// body.appendChild(cloneH1);
+// body.appendChild(cloneH2);
+
+// function sayHello(){
+//     let clone = h1.cloneNode(true);
+//     body.appendChild(clone);
+// }
+
+// setInterval(sayHello, 2000);
+
+// const h1 = document.querySelector("h1");
+// h1.style.color = "white";
+// h1.style.backgroundColor = "green";
+// h1.innerText = "Ezhilarasan Manjula"
+// h1.innerHTML = "<i>vip</i>Ezhilarasan Manjula";
+
+
+// const body = document.querySelector("body");
+// let cloneH1 = h1.cloneNode(true);
+// let cloneH2 = h1.cloneNode(false);
+// body.appendChild(cloneH1);
+// body.appendChild(cloneH2);
+
+
+
+// let divclock = document.querySelector(".devclock");
+// divclock.style.fontSize = "50px";
+
+//  function currentTime(){
+//     let date = new Date()
+//     let hours = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+//     divclock.innerHTML = hours;
+//  }
+// setInterval(currentTime, 1000);
+
+
+
+// classList.add()
+// classList.remove()
+// classList.toggle()
+// setAttribute()
